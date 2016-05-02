8264923c8bb4ee59f8386de3e5e0bce2965624ea
wonhwa <wonhwa@candlemedia.co.kr>
2016-05-02 10:49:00 +0900

    Initial commit

---
+++ src/main/java/com/crazysnoopy/sample/MainTest.java
@@ -0,0 +1,11 @@
+package com.crazysnoopy.sample;
+
+/**
+ * Created by wonhwa on 2016-05-02.
+ */
+public class MainTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world!!");
+    }
+}
