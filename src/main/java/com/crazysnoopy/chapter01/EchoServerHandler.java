5a357c8f47d59e7b2c95775342184e19247fbe8c
wonhwa <wonhwa@candlemedia.co.kr>
2016-05-02 14:51:33 +0900

    EchoServer example

---
+++ src/main/java/com/crazysnoopy/chapter01/EchoServerHandler.java
@@ -0,0 +1,38 @@
+package com.crazysnoopy.chapter01;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
+
+/**
+ * Created by wonhwa on 2016-05-02.
+ */
+@ChannelHandler.Sharable
+public class EchoServerHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf in = (ByteBuf) msg;
+        System.out.println(
+                "Server recevied : " + in.toString(CharsetUtil.UTF_8)
+                // 메세지를 콘솔에 로깅
+        );
+        ctx.write(in);
+        // 아웃바운드 메시지를 플러시하지 않은 채로 받은 메시지를 발신자로 출력함
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        ctx.writeAndFlush(Unpooled.EMPTY_BUFFER).addListener(ChannelFutureListener.CLOSE);
+        // 대기중인 메시지를 원격 피어로 플러시하고 채널을 닫는다.
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
