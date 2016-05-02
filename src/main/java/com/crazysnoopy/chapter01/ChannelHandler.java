5dfcf9a088a887d38d696f17310e1abaa8186084
wonhwa <wonhwa@candlemedia.co.kr>
2016-05-02 11:49:37 +0900

    add README

---
+++ src/main/java/com/crazysnoopy/chapter01/ChannelHandler.java
@@ -0,0 +1,17 @@
+package com.crazysnoopy.chapter01;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * Created by wonhwa on 2016-05-02.
+ */
+public class ChannelHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        //super.channelActive(ctx);
+        System.out.println(
+                "Client " + ctx.channel().remoteAddress() + " connected"
+        );
+    }
+}
